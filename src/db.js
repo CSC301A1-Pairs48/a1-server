d367076023333b9bdbe94133ec07fb0c725e9ff6
TomKan <tomkan0909@gmail.com>
2022-02-09 13:46:49 -0500

    First commit

---
+++ src/db.js
@@ -0,0 +1,204 @@
+const DB = [
+    {
+        title: "CryptoPunk #2890",
+        price: "Ξ 605", 
+        img: "/punk2890.png",
+        metadata: {
+            id: "2890",
+            type: "Human",
+            count: "3",
+            accessories: "Frumpy Hair, Green Eye Shadow, Pipe" 
+        }   
+    },
+    {
+        title: "CryptoPunk #3051",
+        price: "Ξ 84.95", 
+        img: "/punk3051.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+        
+    },
+    {
+        title: "CryptoPunk #3100",
+        price: "Ξ 4200", 
+        img: "/punk3100.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
+    },
+    {
+        title: "CryptoPunk #4085",
+        price: "Ξ 60.95", 
+        img: "/punk4085.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        } 
+    },
+    {
+        title: "CryptoPunk #5217",
+        price: "Ξ 2250", 
+        img: "/punk5217.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #5790",
+        price: "Ξ 72.99", 
+        img: "/punk5790.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #6487",
+        price: "Ξ 550", 
+        img: "/punk6487.png", 
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #6965",
+        price: "Ξ 800", 
+        img: "/punk6965.png", 
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #7252",
+        price: "Ξ 1600", 
+        img: "/punk7252.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
+    },
+    {
+        title: "CryptoPunk #7804",
+        price: "Ξ 4200", 
+        img: "/punk7804.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+
+    },
+    {
+        title: "CryptoPunk #8082",
+        price: "Ξ 40", 
+        img: "/punk8082.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #8348",
+        price: "Ξ 85", 
+        img: "/punk8348.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #8357",
+        price: "Ξ 115", 
+        img: "/punk8357.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #8620",
+        price: "Ξ 400", 
+        img: "/punk8620.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
+    },
+    {
+        title: "CryptoPunk #8805",
+        price: "Ξ 420", 
+        img: "/punk8805.png",   
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #8857",
+        price: "Ξ 2000", 
+        img: "/punk8857.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
+    },
+    {
+        title: "CryptoPunk #9572",
+        price: "Ξ 99.95", 
+        img: "/punk9572.png",   
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+    },
+    {
+        title: "CryptoPunk #9998",
+        price: "Ξ 124457.0675", 
+        img: "/punk9998.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
+    }
+]
+
+export default DB;
